eb71ad0f81edfdca5ee8d3eb550cc82baa570a59
mariamoljames <mariamol.james@gmail.com>
2020-08-17 00:42:35 -0400

    added order_items

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let itemMainDiv = document.querySelector("#items")
+let orderSideBar = document.querySelector("#order-sidebar")
 
 fetch(`http://localhost:3000/items`)
 .then(r => r.json())
@@ -18,7 +19,7 @@ let turnItemIntoHTML = (item) => {
     itemImage.src = item.image
 
     let itemPrice = document.createElement("p")
-    itemPrice.innerHTML = item.price
+    itemPrice.innerHTML = `$ ${item.price}`
 
     let addButon = document.createElement("button")
     addButon.innerText = "Add to cart"
@@ -26,4 +27,45 @@ let turnItemIntoHTML = (item) => {
     itemDiv.append(itemImage, itemName, itemPrice, addButon)
 
     itemMainDiv.append(itemDiv)
-}
+
+    itemImage.addEventListener("click", (event) => {
+        itemMainDiv.innerHTML = ""
+    
+    let itemDescription = document.createElement("p")
+    itemDescription.innerHTML = `Item Description: ${item.description}`
+    
+    let itemCategory = document.createElement("p")
+    itemCategory.innerHTML = `Item Category: ${item.category}`
+
+    itemDiv.append(itemImage, itemName, itemPrice, itemDescription, itemCategory, addButon)
+    
+    itemMainDiv.append(itemDiv)
+
+})
+
+    addButon.addEventListener("click", (event) => {
+        // create a new order item form with the userId and the item clicked by user
+        // 
+    let orderItem = document.createElement("div")
+    orderItem.innerHTML = `<h3>${item.name}</h3>`
+    orderSideBar.append(orderItem)
+
+    fetch(`http://localhost:3000/order_items`, {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+            order_id: 1,
+            item_id: item.id
+        })
+    })
+    .then(r => r.json())
+    .then((newOrderItem) => {
+        console.log(newOrderItem["order_id"])
+    })
+
+
+ })
+
+    }
